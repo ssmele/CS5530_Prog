37522eb64a021f4aa2f0726433484f7249db1700
Stone <ssmele1995@gmail.com>
2017-03-13 19:13:26 -0600

    Please enter the commit message for your changes. Lines starting

--- Uotel/src/Connector.java
+++ Uotel/src/Connector.java
@@ -3,27 +3,31 @@ import java.sql.*;
 public class Connector {
 	public Connection con;
 	public Statement stmt;
-	public Connector() throws Exception {
-		try{
-            // TODO: We gotta change these.
-		 	String userName = "5530user";
-	   		String password = "5530@16";
-	        	String url = "jdbc:mysql://georgia.eng.utah.edu/cs5530db";
-		        Class.forName ("com.mysql.jdbc.Driver").newInstance ();
-        		con = DriverManager.getConnection (url, userName, password);
 
-			//DriverManager.registerDriver (new oracle.jdbc.driver.OracleDriver());
-        	//stmt=con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+	public Connector() throws Exception {
+		try {
+			// TODO: We gotta change these.
+			String userName = "5530u66";
+			String password = "5ttga8pf";
+			String url = "jdbc:mysql://georgia.eng.utah.edu:3306/?user=5530u66";
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			con = DriverManager.getConnection(url, userName, password);
+
+			// DriverManager.registerDriver (new
+			// oracle.jdbc.driver.OracleDriver());
+			// stmt=con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,
+			// ResultSet.CONCUR_UPDATABLE);
 			stmt = con.createStatement();
-			//stmt=con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
-        } catch(Exception e) {
+			// stmt=con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,
+			// ResultSet.CONCUR_UPDATABLE);
+		} catch (Exception e) {
 			System.err.println("Unable to open mysql jdbc connection. The error is as follows,\n");
-            		System.err.println(e.getMessage());
-			throw(e);
+			System.err.println(e.getMessage());
+			throw (e);
 		}
 	}
-	
-	public void closeConnection() throws Exception{
+
+	public void closeConnection() throws Exception {
 		con.close();
 	}
 }
--- Uotel/src/testdriver2.java
+++ Uotel/src/testdriver2.java
@@ -12,7 +12,7 @@ public class testdriver2 {
 		System.out.println("2. enter your own query:");
 		System.out.println("3. exit:");
 		System.out.println("pleasse enter your choice:");
-		//Here is a change
+		// Here is a change
 	}
 
 	public static void displayLogin() {
@@ -36,6 +36,8 @@ public class testdriver2 {
 		try {
 			// remember to replace the password
 			con = new Connector();
+			Querys Q = new Querys();
+			Q.newUser("stone", "stone", "swag", "ues", "sone", true, con.stmt);
 			System.out.println("Database connection established");
 
 			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
@@ -43,7 +45,8 @@ public class testdriver2 {
 			while (true) {
 				if (!loggedIn) {
 					displayLogin();
-					while ((choice = in.readLine()) == null && choice.length() == 0);
+					while ((choice = in.readLine()) == null && choice.length() == 0)
+						;
 					try {
 						c = Integer.parseInt(choice);
 					} catch (Exception e) {
@@ -83,7 +86,8 @@ public class testdriver2 {
 					}
 				} else {
 					displayMenu();
-					while ((choice = in.readLine()) == null && choice.length() == 0);
+					while ((choice = in.readLine()) == null && choice.length() == 0)
+						;
 					try {
 						c = Integer.parseInt(choice);
 					} catch (Exception e) {
@@ -93,9 +97,11 @@ public class testdriver2 {
 						continue;
 					if (c == 1) {
 						System.out.println("please enter a cname:");
-						while ((cname = in.readLine()) == null && cname.length() == 0);
+						while ((cname = in.readLine()) == null && cname.length() == 0)
+							;
 						System.out.println("please enter a dname:");
-						while ((dname = in.readLine()) == null && dname.length() == 0);
+						while ((dname = in.readLine()) == null && dname.length() == 0)
+							;
 						Course course = new Course();
 						System.out.println(course.getCourse(cname, dname, con.stmt));
 					} else if (c == 2) {
